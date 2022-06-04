5f0cce0a4bb65713ac965e7c53846f22f3d992bd
Choudhury Iqbal <iqbalchoudhury2@gmail.com>
2022-06-04 18:28:37 -0400

    16X16

---
+++ javascript.js
@@ -0,0 +1,10 @@
+const div = document.querySelector('.canvas');
+
+for (let i = 1; i < 257; i++) {
+    let childDiv = document.createElement('div');
+    div.appendChild(childDiv);
+    
+}
+
+//Adding 16x16 div
+
