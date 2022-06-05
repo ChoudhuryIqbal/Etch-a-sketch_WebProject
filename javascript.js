fe8a4eebe2a3efdba9ff95bb30d15e916f836ec4
Choudhury Iqbal <iqbalchoudhury2@gmail.com>
2022-06-05 13:20:52 -0400

    migrate to Css Grid

--- javascript.js
+++ javascript.js
@@ -1,10 +1,9 @@
-const div = document.querySelector('.canvas');
+ const div = document.querySelector('.canvas');
 
-for (let i = 1; i < 257; i++) {
-    let childDiv = document.createElement('div');
-    div.appendChild(childDiv);
-    
-}
+ for (let i = 1; i < 626; i++) {
+     let childDiv = document.createElement('div');
+     div.appendChild(childDiv);
 
-//Adding 16x16 div
+ }
 
+ //Adding 16x16 div
