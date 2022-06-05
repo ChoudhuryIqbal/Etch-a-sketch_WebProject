67d31e0bc407254c7a260fd7f7876c015c406a40
Choudhury Iqbal <iqbalchoudhury2@gmail.com>
2022-06-05 18:16:09 -0400

    Added Mouseover

--- javascript.js
+++ javascript.js
@@ -1,9 +1,29 @@
- const div = document.querySelector('.canvas');
+const div = document.querySelector('.canvas');
 
- for (let i = 1; i < 626; i++) {
-     let childDiv = document.createElement('div');
-     div.appendChild(childDiv);
+for (let i = 1; i < 626; i++) {
+    let childDiv = document.createElement('div');
+    childDiv.classList.add('childDiv');
+    div.appendChild(childDiv);
 
- }
 
- //Adding 16x16 div
+}
+const rects = document.querySelectorAll('.childDiv')
+rects.forEach(function(rect) {
+    rect.addEventListener("mouseover", func, false)
+
+    function func() {
+        rect.setAttribute("style", "background-color:green;")
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+//Adding 16x16 div
