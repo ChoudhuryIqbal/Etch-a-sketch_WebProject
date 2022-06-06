f376352ccee57b87dba300edb75dab80383dbc50
Choudhury Iqbal <iqbalchoudhury2@gmail.com>
2022-06-06 01:11:44 -0400

    Main functionality added

--- javascript.js
+++ javascript.js
@@ -21,12 +21,15 @@ rects.forEach(function(rect) {
     /// defautl end
 
 
-function createCanvas(color) {
+function createCanvas(gridCount) {
     //remove Style
     let node = document.querySelectorAll('.childDiv').forEach(e =>
         e.remove());
 
-    for (let i = 1; i < 626; i++) {
+    let limit = (gridCount * gridCount) + 1;
+    let dynamicStyle = "grid-template-rows: repeat(" + gridCount + ", 1fr);grid-template-columns: repeat(" + gridCount + ", 1fr);";
+    div.setAttribute('style', dynamicStyle);
+    for (let i = 1; i < limit; i++) {
         let childDiv = document.createElement('div');
         childDiv.classList.add('childDiv');
         div.appendChild(childDiv);
@@ -38,9 +41,24 @@ function createCanvas(color) {
         rect.addEventListener("mouseover", func, false)
 
         function func() {
+            let colorPicker = document.querySelector("#colorpicker")
+            let color = colorPicker.value;
             rect.setAttribute("style", "background-color:" + color + ";");
+
         }
     })
+}
+
+
+document.querySelector("#btn").addEventListener('click', promptMe);
+let gridSize = 0;
+
+
+function promptMe() {
+    let userInput = prompt("Please enter the size of Grid? (choose a number between 16-100)");
+    gridSize = userInput;
+
+    createCanvas(gridSize)
 }
 
 
